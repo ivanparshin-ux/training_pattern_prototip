cfd6a77ce7c497a22077beb61c1e9854efb7f380
ivanparshin-ux <ivan2077o@icloud.com>
2025-02-05 20:12:42 +0300

    Update Program.cs

--- training_pattern_prototip/training_pattern_prototip/Program.cs
+++ training_pattern_prototip/training_pattern_prototip/Program.cs
@@ -1,114 +1,120 @@
-﻿using System;
 public abstract class Shape
 {
-    protected int x, y;
-    protected string color;
+    public int X { get; set; }
+    public int Y { get; set; }
+    public string Color { get; set; }
 
-    public Shape(int x, int y, string color)
+    protected Shape() { }
+
+    protected Shape(Shape source)
     {
-        this.x = x;
-        this.y = y;
-        this.color = color;
+        X = source.X;
+        Y = source.Y;
+        Color = source.Color;
     }
+
     public abstract Shape Clone();
-    public override string ToString()
-    {
-        return $"Shape at ({x}, {y}) with color {color}";
-    }
 }
+
 public class Rectangle : Shape
 {
-    private int width, height;
+    public int Width { get; set; }
+    public int Height { get; set; }
 
-    public Rectangle(int x, int y, string color, int width, int height)
-        : base(x, y, color)
+    public Rectangle() { }
+
+    public Rectangle(Rectangle source) : base(source)
     {
-        this.width = width;
-        this.height = height;
+        Width = source.Width;
+        Height = source.Height;
     }
+
     public override Shape Clone()
     {
-        return new Rectangle(x, y, color, width, height);
-    }
-
-    public override string ToString()
-    {
-        return $"{base.ToString()}, Width: {width}, Height: {height}";
+        return new Rectangle(this);
     }
 }
+
 public class Circle : Shape
 {
-    private int radius;
+    public int Radius { get; set; }
 
-    public Circle(int x, int y, string color, int radius)
-        : base(x, y, color)
+    public Circle() { }
+
+    public Circle(Circle source) : base(source)
     {
-        this.radius = radius;
+        Radius = source.Radius;
     }
+
     public override Shape Clone()
     {
-        return new Circle(x, y, color, radius);
-    }
-    public override string ToString()
-    {
-        return $"{base.ToString()}, Radius: {radius}";
+        return new Circle(this);
     }
 }
+
 public class Cube : Shape
 {
-    private int side;
-    public Cube(int x, int y, string color, int side)
-        : base(x, y, color)
+    public int Volume { get; set; }
+
+    public Cube() { }
+
+    public Cube(Cube source) : base(source)
     {
-        this.side = side;
+        Volume = source.Volume;
     }
+
     public override Shape Clone()
     {
-        return new Cube(x, y, color, side);
-    }
-    public override string ToString()
-    {
-        return $"{base.ToString()}, Side: {side}";
+        return new Cube(this);
     }
 }
+
 public class Sphere : Shape
 {
-    private int radius;
+    public int Radius { get; set; }
 
-    public Sphere(int x, int y, string color, int radius)
-        : base(x, y, color)
+    public Sphere() { }
+
+    public Sphere(Sphere source) : base(source)
     {
-        this.radius = radius;
+        Radius = source.Radius;
     }
+
     public override Shape Clone()
     {
-        return new Sphere(x, y, color, radius);
-    }
-
-    public override string ToString()
-    {
-        return $"{base.ToString()}, Radius: {radius}";
+        return new Sphere(this);
     }
 }
+
+public class Application
+{
+    public void Run()
+    {
+        // Пример использования
+        Rectangle rectangle = new Rectangle { X = 10, Y = 15, Color = "Red", Height = 12, Width = 12 };
+        Console.WriteLine($"Rectangle: X={rectangle.X}, Y={rectangle.Y}, Color={rectangle.Color}, Height={rectangle.Height}, Width={rectangle.Width}");
+        Console.WriteLine();
+
+        Circle circle = new Circle { X = 3, Y = 2, Color = "Green", Radius = 8 };
+        Console.WriteLine($"Circle: X = {circle.X}, Y = {circle.Y}, Color = {circle.Color}, Radius = {circle.Radius}");
+        Console.WriteLine();
+
+        Sphere sphere = new Sphere { X = 43, Y = 32, Color = "Yellow", Radius = 7 };
+        Console.WriteLine($"Sphere: X = {sphere.X}, Y = {sphere.Y}, Color = {sphere.Color}, Radius = {sphere.Radius}");
+        Console.WriteLine();
+
+        Cube cube = new Cube { X = 0, Y = 0, Color = "White", Volume = 13 };
+        Console.WriteLine($"Cube: X = {cube.X}, Y = {cube.Y}, Color = {cube.Color}, Volume = {cube.Volume}");
+        Console.WriteLine();
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Rectangle originalRectangle = new Rectangle(10, 20, "Red", 50, 30);
-        Circle originalCircle = new Circle(15, 25, "Blue", 20);
-        Cube originalCube = new Cube(20, 30, "Green", 40);
-        Sphere originalSphere = new Sphere(25, 35, "Yellow", 30);
-        Shape clonedRectangle = originalRectangle.Clone();
-        Shape clonedCircle = originalCircle.Clone();
-        Shape clonedCube = originalCube.Clone();
-        Shape clonedSphere = originalSphere.Clone();
-        Console.WriteLine("Original Rectangle: " + originalRectangle);
-        Console.WriteLine("Cloned Rectangle: " + clonedRectangle);
-        Console.WriteLine("Original Circle: " + originalCircle);
-        Console.WriteLine("Cloned Circle: " + clonedCircle);
-        Console.WriteLine("Original Cube: " + originalCube);
-        Console.WriteLine("Cloned Cube: " + clonedCube);
-        Console.WriteLine("Original Sphere: " + originalSphere);
-        Console.WriteLine("Cloned Sphere: " + clonedSphere);
+        Application app = new Application();
+        app.Run();
+        Console.ReadKey();
     }
 }
